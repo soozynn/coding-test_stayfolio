4bf14421ca64e01465038cb3705774e91f7840a7
soozynn <81217456+soozynn@users.noreply.github.com>
2022-07-25 00:23:37 +0900

    complete

--- test/04-더하고 뺴기.test.js
+++ test/04-더하고 뺴기.test.js
@@ -3,10 +3,7 @@ import solution from "../lib/04-더하고 빼기";
 
 describe("04. 더하고 빼기 테스트", () => {
   it("It should pass basic cases", () => {
+    expect(solution(23)).to.equal("apple");
     expect(solution(10)).to.equal("apple");
-  });
-
-  it("10~10000 사이의 숫자가 아닐 경우", () => {
-    expect(solution(100001)).to.equal(undefined);
   });
 });
